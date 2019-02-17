89c02922018928d53e33cebb4576e4e636434023
rjansen <raphael.jansen@resultadosdigitais.com.br>
2019-02-17 04:25:50 -0300

    Update function module description and fix configuration

--- function/function.go
+++ function/function.go
@@ -1,8 +1,16 @@
 package server
 
 import (
+	stdsql "database/sql"
 	"net/http"
 	"sync"
+
+	"github.com/rjansen/fivecolors/core/api"
+	"github.com/rjansen/fivecolors/core/graphql"
+	"github.com/rjansen/l"
+	"github.com/rjansen/migi"
+	"github.com/rjansen/raizel/sql"
+	"github.com/rjansen/yggdrasil"
 )
 
 var (
@@ -10,31 +18,67 @@ var (
 	serverHandler http.HandlerFunc
 )
 
-/*
+type options struct {
+	driver string
+	dsn    string
+}
+
+func newOptions() options {
+	var (
+		env     = migi.NewOptions(migi.NewEnvironmentSource())
+		options options
+	)
+	env.StringVar(
+		&options.driver, "raizel_driver", "postgres", "Raizel database driver",
+	)
+	env.StringVar(
+		&options.dsn,
+		"raizel_dsn",
+		"postgres://postgres:@127.0.0.1:5432/postgres?sslmode=disable",
+		"Raizel datasource name format",
+	)
+	env.Parse()
+	return options
+}
+
 func newTree() yggdrasil.Tree {
-	schema := mockschema.New()
-	roots := yggdrasil.NewRoots()
-	err := migi.Register(&roots, migi.NewOptions(migi.NewEnvironmentSource()))
+	var (
+		options   = newOptions()
+		logger    = l.NewZapLoggerDefault()
+		roots     = yggdrasil.NewRoots()
+		err       error
+		sqlDriver string
+		sqlDsn    string
+	)
+
+	err = l.Register(&roots, logger)
 	if err != nil {
 		panic(err)
 	}
-	err = l.Register(&roots, l.NewZapLoggerDefault())
+	sqlDB, err := stdsql.Open(options.driver, options.dsn)
 	if err != nil {
 		panic(err)
 	}
-	err = graphql.Register(&roots, schema)
+	db, err := sql.NewDB(sqlDB)
+	if err != nil {
+		panic(err)
+	}
+	err = sql.Register(&roots, db)
+	if err != nil {
+		panic(err)
+	}
+	err = graphql.Register(&roots, NewSchema(roots.NewTreeDefault()))
 	if err != nil {
 		panic(err)
 	}
 	return roots.NewTreeDefault()
 }
-*/
 
 func Handler(w http.ResponseWriter, r *http.Request) {
-	// once.Do(
-	// 	func() {
-	// 		serverHandler = api.NewGraphQLHandler(newTree())
-	// 	},
-	// )
-	// serverHandler(w, r)
+	once.Do(
+		func() {
+			serverHandler = api.NewGraphQLHandler(newTree())
+		},
+	)
+	serverHandler(w, r)
 }
--- function/go.mod
+++ function/go.mod
@@ -1,9 +1,10 @@
-module github.com/rjansen/fivecolors/server
+module github.com/rjansen/fivecolors/function
 
 require (
 	github.com/rjansen/fivecolors/core v0.0.9
 	github.com/rjansen/l v0.0.0-20190127231537-1cd311bd3239
 	github.com/rjansen/migi v0.0.0-20190113201353-309b13ecc08f
+	github.com/rjansen/raizel v0.0.1
 	github.com/rjansen/yggdrasil v0.0.0-20190113202223-f5e7a218aac6
 	github.com/stretchr/testify v1.3.0
 )
